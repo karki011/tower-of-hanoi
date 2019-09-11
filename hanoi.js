2cc029c241a109a1df6545ef389e885af31992e2
Binup karki <karki011@gmail.com>
2019-09-11 09:09:04 -0500

    game working

--- hanoi.js
+++ hanoi.js
@@ -5,17 +5,59 @@ const disk_3 = document.getElementById("disk3");
 const disk_4 = document.getElementById("disk4");
 const tower_2 = document.getElementById("tower_2");
 const tower_3 = document.getElementById("tower_3");
+let towers = document.querySelectorAll(".tower");
 
-const main = function () {
-    tower_1.addEventListener('click', function () {
-        console.log("Tower 1");
-    });
-    tower_2.addEventListener('click', function () {
-        console.log("Tower 2");
-    });
-    tower_3.addEventListener('click', function () {
-        console.log("Tower 3");
-    });
+let playerMode = "Pickup";
+let hold = null;
+
+
+for (var i = 0; i < towers.length; i++) {
+    towers[i].addEventListener('click', isPickupIsputToDestinationTower);
 }
 
-main();
+function isPickupIsputToDestinationTower(event) {
+    console.log(playerMode);
+    if (playerMode === "Pickup") {
+        pickFromSourceTower(event.currentTarget);
+    } else if (playerMode === "Release") {
+        putToDestinationTower(event.currentTarget);
+    }
+}
+
+
+const pickFromSourceTower = function (initalDiv) {
+    let numberOfDivs = initalDiv.childElementCount;
+    console.log(numberOfDivs);
+    if (numberOfDivs === 0) {
+        alert("This tower is empty, Click another tower");
+    } else {
+        hold = initalDiv.lastElementChild;
+        playerMode = "Release"
+    }
+    win();
+}
+
+const putToDestinationTower = function (lastDiv) {
+    let lastElement = lastDiv.lastElementChild;
+    if (!lastElement) {
+        lastDiv.appendChild(hold);
+        playerMode = "Pickup"
+    } else {
+        let currentDiskWidth = lastElement.clientWidth;
+        let playerHoldDisk = hold.clientWidth;
+        if (currentDiskWidth <= playerHoldDisk) {
+            alert("NO NO NO! Disk too big, try another tower.")
+            playerMode = "Pickup"
+        } else {
+            lastDiv.appendChild(hold);
+            playerMode = "Pickup"
+        }
+    }
+    win();
+}
+
+const win = function () {
+    if (tower_1.childElementCount === 0 && tower_3.childElementCount === 4) {
+        alert('You Won')
+    }
+}
