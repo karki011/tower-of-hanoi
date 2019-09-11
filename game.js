99198bac44985e8fc20f67fea09265ec946bcef2
Binup karki <karki011@gmail.com>
2019-09-10 23:17:50 -0500

    tower and disk created

--- game.js
+++ game.js
@@ -1,15 +1,18 @@
+// let nDisk;
+// let start;
+// let destination;
+// let middle;
 
 
- const tower_move = function (nDisk, start, destination, middle) {
-     if( nDisk === 1){
-        console.log(`Move disk ${nDisk} ${start} - ${destination}`);
-     } else {
-        tower_move(nDisk-1, start, middle, destination)
-        console.log(`Move disk ${nDisk } ${start}  -  ${destination}`);
-        tower_move(nDisk-1, middle, destination, start)
-     }
- }
+//  const tower_move = function (nDisk, start, destination, middle) {
+//      if( nDisk === 1){
+//         console.log(`Move disk ${nDisk} ${start} - ${destination}`);
+//      } else {
+//         tower_move(nDisk-1, start, middle, destination)
+//         console.log(`Move disk ${nDisk } ${start}  -  ${destination}`);
+//         tower_move(nDisk-1, middle, destination, start)
+//      }
+//  }
  
-tower_move(3, "A", "C", "B");
+// tower_move(4, "A", "C", "B");
 
- 
---
+++ hanoi.js
@@ -0,0 +1,8 @@
+const tower_1 = document.getElementById("tower_1");
+const disk_1 = document.getElementById("disk1");
+const disk_2 = document.getElementById("disk2");
+const disk_3 = document.getElementById("disk3");
+const disk_4 = document.getElementById("disk4");
+const tower_2 = document.getElementById("tower_2");
+const tower_3 = document.getElementById("tower_3");
+
