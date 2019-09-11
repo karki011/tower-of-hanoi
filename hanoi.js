85dd49414caf2b5b01ab4159665cac7a33736abf
Binup karki <karki011@gmail.com>
2019-09-10 23:30:02 -0500

    Added eventlisterner to tower

--- hanoi.js
+++ hanoi.js
@@ -6,3 +6,16 @@ const disk_4 = document.getElementById("disk4");
 const tower_2 = document.getElementById("tower_2");
 const tower_3 = document.getElementById("tower_3");
 
+const main = function () {
+    tower_1.addEventListener('click', function () {
+        console.log("Tower 1");
+    });
+    tower_2.addEventListener('click', function () {
+        console.log("Tower 2");
+    });
+    tower_3.addEventListener('click', function () {
+        console.log("Tower 3");
+    });
+}
+
+main();
